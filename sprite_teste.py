150281fb8163bae248288cb932f22c0515c6d0a7
Wansel <wansel@live.com>
2016-10-10 21:30:23 -0300

    Inicio da implementação da classe pato

--- sprite_teste.py
+++ sprite_teste.py
@@ -1,6 +1,39 @@
 # -*- coding: utf-8 -*-
-from pygame.locals import *
-
+#from pygame.locals import *
+from random import randint
 #Inicia o PyGame
-pygame.init()
+#pygame.init()
+lvl_velocidade = 3
+lvl_tamanho = 100
+
+class Pato:
+	#velocidade
+	#tamanho
+	#ponto_de_vida
+	#cor
+	def __init__(self,velocidade, cor, ponto_de_vida, tamanho):
+		self.velocidade = lvl_velocidade * randint(1,3)
+		self.cor = 0
+		self.ponto_de_vida = 1
+		self.tamanho = lvl_tamanho * float(randint(7,13)/float(10))
+	def getVelocidade():
+		return self.velocidade
+	def stringVelocidade():
+		return "Pato tem {} de velocidade".format(self.getVelocidade)
+
+patoA = Pato(lvl_velocidade, 0, 1, lvl_tamanho)
+patoB = Pato(lvl_velocidade, 0, 1, lvl_tamanho)
+patoC = Pato(lvl_velocidade, 0, 1, lvl_tamanho)
+
+print patoA.velocidade
+print patoA.tamanho
+print patoA.cor
+
+print patoB.velocidade
+print patoB.tamanho
+print patoB.cor
+
+print patoC.velocidade
+print patoC.tamanho
+print patoC.cor
 
